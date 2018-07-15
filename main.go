7c3c8f7beafac893c38a2f24f32799e194b216c4
Zee Abrahams <zee.j.abrahams@gmail.com>
2018-07-15 17:02:27 -0600

    Gets program loaded

--- main.go
+++ main.go
@@ -1,36 +1,58 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+const (
+	PROGRAM_OFFSET = 512
 )
 
 type Chip8 struct {
-    memory []byte
+	memory    []byte
 	registers map[byte]byte
+	regI      uint16
 }
 
 func NewChip8() *Chip8 {
 	m := make([]byte, 4096, 4096)
 	r := map[byte]byte{}
-	// Registers 0-15 are for V0-VF. 16 is I.
-	for i := 0; i < 17; i++ {
+	// Registers 0-15 are for V0-VF.
+	for i := 0; i < 16; i++ {
 		r[byte(i)] = byte(0)
 	}
 
 	return &Chip8{
-		memory: m,
+		memory:    m,
 		registers: r,
+		regI:      0,
 	}
 }
 
-func (*Chip8) LoadProgram (filename string) {
+func (c8 *Chip8) LoadProgram(filename string) {
 	fmt.Printf("Loading Program From File: %s\n", filename)
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("could not load program file: %v", err)
+	}
+	defer file.Close()
+	binData, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("error reading data from file: %v")
+	}
+
+	for i := 0; i < len(binData); i++ {
+		c8.memory[PROGRAM_OFFSET+i] = binData[i]
+	}
+	fmt.Printf("Finshed loading program. Loaded %d bytes\n", len(binData))
 }
 
 func main() {
 	fmt.Println("Starting Chip8 Emulator")
 	chip8 := NewChip8()
-	chip8.LoadProgram("test.com")
+	chip8.LoadProgram("/Users/zach/chip8/ibm_logo.ch8")
 	fmt.Println("Closing Chip8 Emulator")
 }
-
