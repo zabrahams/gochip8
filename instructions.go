fcf734a0a03df93128c8e0dd4949ea8fe0747292
Zee Abrahams <zee.j.abrahams@gmail.com>
2018-07-17 00:14:29 -0600

    Adds support for horizontal wrapping on draw

--- instructions.go
+++ instructions.go
@@ -19,7 +19,7 @@ func (c8 *Chip8) execInstr() {
 	// 6xkk - LD Vx, byte - Load the byte value into the register specified by x
 	case lNib(instr[0]) == x6:
 		c8.registers[rNib(instr[0])] = instr[1]
-		// 7xkk - ADD Vx, byte
+	// 7xkk - ADD Vx, byte
 	case lNib(instr[0]) == x7:
 		c8.registers[rNib(instr[0])] += instr[1]
 	// Annn - LD I, addr - Load the int16 addr specified by nnn into the I register
@@ -30,7 +30,7 @@ func (c8 *Chip8) execInstr() {
 	// values of Vx and Vy. If at least one pixel is erased set VF to 1 otherwise to 0
 	// if a part of the sprite is located off screen - wrap it.
 	case lNib(instr[0]) == xD:
-		xOffset := 55 - c8.registers[rNib(instr[0])]
+		xOffset := 55 - int(c8.registers[rNib(instr[0])])
 		yOffset := c8.registers[lNib(instr[1])]
 		length := rNib(instr[1])
 
@@ -39,11 +39,19 @@ func (c8 *Chip8) execInstr() {
 		//	fmt.Printf("%08b\n", line)
 		//}
 
-		// worry about horizontal wrapping later
 		c8.registers[xF] = 0
 		for i := 0; i < int(length); i++ {
 			var spriteRow uint64
-			spriteRow = uint64(sprite[i]) << xOffset
+			// if we need to wrap
+			if xOffset < 0 {
+				unWrappedOffset := xOffset * -1
+				unWrapped := uint64(sprite[i]) >> (uint(unWrappedOffset))
+				wrapped := uint64(sprite[i]) << uint(64+xOffset)
+				spriteRow = unWrapped ^ wrapped
+
+			} else {
+				spriteRow = uint64(sprite[i]) << uint(xOffset)
+			}
 			y := (yOffset + byte(i)) % 32
 			currentRow := c8.display.screen[y]
 
